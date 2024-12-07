fc1a1c09a68666781086a4fff2923b1df08c0ef7
AidaTegeran <aidategeran@gmail.com>
2024-12-06 21:13:55 -0600

    create captureGroup

--- src/Section3/Exercises/Regex.java
+++ src/Section3/Exercises/Regex.java
@@ -5,17 +5,26 @@ import java.util.regex.Pattern;
 
 public class Regex {
     public static void main(String[] args) {
-        String nameOfDogs = "Dark, bark, Lark, stark";
+        //String nameOfDogs = "Dark, bark, Lark, stark";
+        //String abracadabra = "Abracadabra, Agracadagra";
+        String address = "1659 Eagle Grove Ct. apt 521";
 
-        String regex = "\\b" + nameOfDogs + "\\b";
+
+        String regex = "(?<numberOfStreet>\\d{1,4})\\s(?<nameOfStreet>\\w+\\b)(?<numberOfApt>\\w+\\s\\d{1,4})";
+        //String regex = "\\b" + nameOfDogs + "\\b";
+        //String regex = "A[bg]racada[bg]ra";
+
 
         Pattern pat = Pattern.compile(regex);
-        Matcher mat = pat.matcher(nameOfDogs);
+        Matcher mat = pat.matcher(address);
 
-        if (mat.find()) {
-            System.out.println(mat.group());
-
+        if (mat.matches()) {
+            System.out.println(mat.group("numberOfStreet"));
+            //System.out.println(mat.group("nameOfStreet"));
+            //System.out.println(mat.group("numberOfApt"));
         }
+
+
 
 
     }
