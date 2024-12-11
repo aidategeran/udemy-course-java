56dc108c447315af91d233aa784089f137cd1b5e
AidaTegeran <aidategeran@gmail.com>
2024-12-10 21:30:46 -0600

    create captureGroup

---
+++ src/Section5/DataType.java
@@ -0,0 +1,10 @@
+package Section5;
+
+public class DataType {
+    byte myByte = 127;
+    short myShort = 127;
+    int myInt = 127;
+    long myLong = 127L;
+    float myFloat = 127.0f;
+    double myDouble = 127.0;
+}
