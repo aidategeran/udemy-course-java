1bb2c2515c4104f76e23b90c018df58869eebae3
AidaTegeran <aidategeran@gmail.com>
2024-12-29 19:35:41 -0600

    create captureGroup

---
+++ src/Section5/NumericWrapperClass.java
@@ -0,0 +1,14 @@
+package Section5;
+
+public class NumericWrapperClass {
+    public static void main(String[] args) {
+        int age = Integer.parseInt(args[0]);
+        System.out.printf("You will be %d years of age in 15 years.%n", age + 15);
+
+    }
+
+
+    public static void storeData(Object obj) {
+        // do something with obj
+    }
+}
