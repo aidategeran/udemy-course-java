6d38af8f4e8f5dd6e59f09d2affb6b7d7282f907
AidaTegeran <aidategeran@gmail.com>
2025-01-04 21:33:14 -0600

    create captureGroup

---
+++ src/Section5/NameList.java
@@ -0,0 +1,43 @@
+package Section5;
+
+public class NameList {
+    private String [] names;
+    private int currentIndex;
+
+
+    public String[] getNames() {
+        return names;
+    }
+
+    public void setNames(String[] names) {
+        this.names = names;
+    }
+
+    public NameList(){
+        names = new String[]{
+                "alice", "bob", "carol", "dave", "eve",
+                "frank", "grace", "heidi", "ivan", "judy"
+        };
+        currentIndex = 0;
+    }
+
+    public String next(){
+        if (currentIndex < names.length){
+            return names[currentIndex++];
+        } else {
+            return "No more names";
+        }
+    }
+
+    public static void main(String[] args) {
+        NameList nameList = new NameList();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(nameList.next());
+        }
+
+        System.out.println(nameList.next());
+
+    }
+}
+
+
