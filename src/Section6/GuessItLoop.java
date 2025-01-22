4525e52276dd24844943d17b074b9e9a04be70d8
AidaTegeran <aidategeran@gmail.com>
2025-01-22 13:04:40 -0600

    create captureGroup

--- src/Section6/GuessItLoop.java
+++ src/Section6/GuessItLoop.java
@@ -5,10 +5,14 @@ import java.util.Random;
 public class GuessItLoop {
     public static void main(String[] args) {
         int randomNum = new Random ().nextInt(10)+1;
-        String guessedNumText = System.console().readLine("Please guess a number between 1 and 10 inclusively");
+        String guessedNumText = System.console().readLine("Please guess a number between 1 and 10 inclusively: ");
         int guessedNum = Integer.parseInt(guessedNumText);
-
-
+        if (guessedNum == randomNum) {
+            System.out.printf("The random number was %d. You got it!%n", randomNum);
+            return;
+        } else {
+            System.out.println("You didn't get it");
+        }
 
     }
 }
