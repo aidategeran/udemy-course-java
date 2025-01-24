fe59b7026dafefb93e78fb0113a5e548741db578
AidaTegeran <aidategeran@gmail.com>
2025-01-24 12:12:25 -0600

    create captureGroup

--- src/Section6/GuessItLoop.java
+++ src/Section6/GuessItLoop.java
@@ -5,16 +5,36 @@ import java.util.Random;
 public class GuessItLoop {
     public static void main(String[] args) {
         int randomNum = new Random ().nextInt(10)+1;
-        while (true) {
-            String guessedNumText = System.console().readLine("Please guess a number between 1 and 10 inclusively: ");
-            int guessedNum = Integer.parseInt(guessedNumText);
-            if (guessedNum == randomNum) {
-                System.out.printf("The random number was %d. You got it!%n", randomNum);
-                return;
-            } else {
-                System.out.println("You didn't get it");
+        String guessedNumText = null;
+        int wrongGuessCount = 1;
+//        while (!"q".equals(guessedNumText)) {
+//            guessedNumText = System.console().readLine("Please guess a number between 1 and 10 inclusively: ");
+//
+//            if (guessedNumText.matches("-?\\d{1,3}")) {
+//                int guessedNum = Integer.parseInt(guessedNumText);
+//                if (guessedNum == randomNum) {
+//                    System.out.printf("The random number was %d. You got it!%n", randomNum);
+//                    return;
+//                } else {
+//                    System.out.println("You didn't get it");
+//                }
+//            }
+//        }
+        do {
+            guessedNumText = System.console().readLine("Please guess a number between 1 and 10 inclusively: ");
+
+            if (guessedNumText.matches("-?\\d{1,3}")) {
+                int guessedNum = Integer.parseInt(guessedNumText);
+                if (guessedNum == randomNum) {
+                    System.out.printf("The random number was %d. You got it!%n", randomNum);
+                    return;
+                } else {
+                    System.out.println("You didn't get it");
+                    wrongGuessCount++;
+                    System.out.printf("You have %d wrong guesses left%n", wrongGuessCount);
+                }
             }
-        }
+        } while (!"q".equals(guessedNumText));
 
     }
 }
