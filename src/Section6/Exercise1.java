a93947f39c0cd172e5662881ac293bcb031260d7
AidaTegeran <aidategeran@gmail.com>
2025-01-29 15:04:04 -0600

    create captureGroup

--- src/Section6/Exercise1.java
+++ src/Section6/Exercise1.java
@@ -2,19 +2,14 @@ package Section6;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        String [] weeks = {"Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"};
+        String[] weeks = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
         int i;
-        for ( i = 0; i < weeks.length; i++) {
-            if (i%2 != 0) {
+        for (i = 0; i < weeks.length; i++) {
+            if (i % 2 != 0) {
                 System.out.println("Today is " + weeks[i].toUpperCase());
                 continue;
             }
-//            String upperWeek = i%2 != 0 ? weeks[i] : weeks[i].toUpperCase();
-//                    System.out.println("Today is " + upperWeek);
-//            break;
             System.out.printf("Today is %s%n", weeks[i]);
         }
-//        for (String dayOfTheWeek : weeks)
-//            System.out.printf("Today is %s%n", dayOfTheWeek);
     }
 }
---
+++ src/Section6/Exercise2.java
@@ -0,0 +1,51 @@
+package Section6;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        String[] weeks = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+//        int i = 0;
+
+        // While loop
+//        while ( i<= weeks.length-1) {
+//            System.out.println("Today is " + weeks[i]);
+//            i++;
+//        }
+
+        // Do while loop
+//        do {
+//            System.out.println("Today is " + weeks[i]);
+//            i++;
+//        } while (i <= weeks.length-1);
+
+
+        // Enhanced for loop
+//        for (String dayOfTheWeek : weeks)
+//            if (dayOfTheWeek.equals("Monday")) {
+//                System.out.println("We eat spaghetti on " + dayOfTheWeek);
+//            } else if (dayOfTheWeek.equals("Tuesday")) {
+//                System.out.println("We eat tacos on " + dayOfTheWeek);
+//            } else if (dayOfTheWeek.equals("Wednesday")) {
+//                System.out.println("We eat chicken on " + dayOfTheWeek);
+//            } else if (dayOfTheWeek.equals("Thursday")) {
+//                System.out.println("We eat meatloaf on " + dayOfTheWeek);
+//            } else if (dayOfTheWeek.equals("Friday")) {
+//                System.out.println("We eat hamburgers on " + dayOfTheWeek);
+//            } else if (dayOfTheWeek.equals("Saturday")) {
+//                System.out.println("We eat pizza on " + dayOfTheWeek);
+//            } else if (dayOfTheWeek.equals("Sunday")) {
+//                System.out.println("We eat pot roast on " + dayOfTheWeek);
+//            }
+
+        // Switch case
+        switch () {
+
+        }
+
+
+
+
+
+
+    }
+
+}
